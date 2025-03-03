991444e63bb831d670fac98a5577985d1c71289e
mehdi <mohamedmehdi.khamassi@esprit.tn>
2025-03-03 09:03:02 +0100

    controller

---
+++ src/main/java/tn/esprit/spring/test/springapp.java
@@ -0,0 +1,9 @@
+package tn.esprit.spring.test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class springapp {
+    public static void main(String[] args) {
+        SpringApplication.run(springapp.class, args);
+    }
+}
