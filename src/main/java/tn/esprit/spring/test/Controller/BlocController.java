cc447c658f14513734930cf255cae8ebb2612652
mehdi <mohamedmehdi.khamassi@esprit.tn>
2025-03-03 09:03:16 +0100

    controller

--- src/main/java/tn/esprit/spring/test/Controller/BlocController.java
+++ src/main/java/tn/esprit/spring/test/Controller/BlocController.java
@@ -1,2 +1,38 @@
-package tn.esprit.spring.test.Controller;public class BlocController {
+package tn.esprit.spring.test.Controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.spring.test.Entity.Bloc;
+import tn.esprit.spring.test.ServiceInterface.IServiceBloc;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+public class BlocController {
+    IServiceBloc serviceBloc;
+    @GetMapping("/getBlocs")
+    public List<Bloc> retrieveBlocs() {
+        return serviceBloc.retrieveBlocs();
+    }
+    @PutMapping("/blocUpdate")
+    public Bloc updateBloc(@RequestBody Bloc bloc) {
+        return serviceBloc.updateBloc(bloc);
+    }
+    @PostMapping("/add")
+    public Bloc addBloc(@RequestBody Bloc bloc) {
+        return serviceBloc.addBloc(bloc);
+    }
+    @GetMapping("/blocID/{idBloc}")
+    public Bloc retrieveBloc(@PathVariable long idBloc) {
+        return serviceBloc.retrieveBloc(idBloc);
+    }
+    @DeleteMapping("/delete/{idBloc}")
+    public void removeBloc(@PathVariable long idBloc) {
+        serviceBloc.removeBloc(idBloc);
+    }
+
+    public List<Bloc> getByUnivNom(String Nom) {
+        return serviceBloc.getByUnivNom(Nom);
+    }
 }
